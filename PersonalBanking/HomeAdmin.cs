172bdaa467d73307848b7187214fad3c03380813
V014 <wangakanjala@gmail.com>
2022-07-12 22:33:51 +0200

    Added admin homepage

--- PersonalBanking/HomeAdmin.cs
+++ PersonalBanking/HomeAdmin.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PersonalBanking
 {
-    public partial class Welcome : UserControl
+    public partial class HomeAdmin : Form
     {
-        public Welcome()
+        public HomeAdmin()
         {
             InitializeComponent();
         }
